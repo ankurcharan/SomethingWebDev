af50e021f610b423a3df075d0caa158f99861098
Ankur Charan <ankurcharan98@gmail.com>
2019-04-06 21:28:16 +0530

    json wali file
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

---
+++ JSON.js
@@ -0,0 +1,38 @@
+// let x = {
+// 	"stringKey": "valueCanBeOfAnyType"
+// }
+
+// let x = {
+// 	key: 20,
+// 	20: 30
+// }
+
+// console.log(x);
+
+
+
+//print krane ke 2 tareeke
+// console.log(x.key);
+// console.log(x["stringKey"]);
+// console.log(x["20"]);
+
+
+
+
+let x = {
+	key2: 20
+}
+x.prerna = 7.6;
+x.key = "value";
+
+// console.log(x);
+
+let y = {
+	key: 20,
+	key2: 30,
+	key3:4040
+}
+y.key = 25;
+
+console.log(y.y);
+
